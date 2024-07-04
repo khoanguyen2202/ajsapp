94f23cbd2af990f652d91d0676cf9bae637ba962
Khoa Nguyen <khoanguyen1799x@gmail.com>
2024-07-04 11:17:59 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  CurrencyPipe,
+  NgFor,
+  NgIf,
+  NgSwitch,
+  NgSwitchCase,
+} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+    NgIf,
+    NgSwitch,
+    NgSwitchCase,
+    NgFor,
+    CurrencyPipe,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ajsapp';
+  title = 'Hello App Component';
+  loginF: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(16),
+    ]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  students: Array<any> = [];
+  onSubmit() {
+    if (this.loginF.invalid) {
+      return;
+    }
+    this.students.push(this.loginF.value);
+  }
 }
